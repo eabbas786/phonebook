6b09e4339f4c7535ace99348833c84a9629aa9c4
Elsa Abbas <eabbas@berkeley.edu>
2026-01-06 11:44:43 -0800

    Add deployed host to allowed hostsin vite config

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -3,8 +3,12 @@ import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
+  plugins: [react()],
   server: {
     host: true, // Listen on 0.0.0.0
     port: 5173, // Or your preferred port
+    allowedHosts: [
+      'phonebook-frontend-24ef.onrender.com'
+    ]
   }
 });
